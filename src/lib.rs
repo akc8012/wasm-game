1bd82c650e873a5fb22dd4a6acd307cf4dbd16f2
Andrew Colannino <akc8012@gmail.com>
2020-05-05 23:18:09 -0400

    big refactor - methods and stuffff

--- src/lib.rs
+++ src/lib.rs
@@ -3,58 +3,77 @@ use std::rc::Rc;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 
+type Canvas = web_sys::HtmlCanvasElement;
+type Context = web_sys::CanvasRenderingContext2d;
+
 #[wasm_bindgen(start)]
 pub fn start() -> Result<(), JsValue> {
+	let canvas = create_canvas()?;
+	let context = Rc::new(create_context(&canvas)?);
+	let pressed = Rc::new(Cell::new(false));
+
+	mouse_down(context.clone(), &canvas, pressed.clone())?;
+	mouse_move(context.clone(), &canvas, pressed.clone())?;
+	mouse_up(context.clone(), &canvas, pressed.clone())?;
+
+	Ok(())
+}
+
+fn create_canvas() -> Result<Canvas, JsValue> {
 	let document = web_sys::window().unwrap().document().unwrap();
-	let canvas = document
-		.create_element("canvas")?
-		.dyn_into::<web_sys::HtmlCanvasElement>()?;
+	let canvas = document.create_element("canvas")?.dyn_into::<Canvas>()?;
+
 	document.body().unwrap().append_child(&canvas)?;
 	canvas.set_width(640);
 	canvas.set_height(480);
 	canvas.style().set_property("border", "solid")?;
-	let context = canvas
-		.get_context("2d")?
-		.unwrap()
-		.dyn_into::<web_sys::CanvasRenderingContext2d>()?;
-	let context = Rc::new(context);
-	let pressed = Rc::new(Cell::new(false));
-	{
-		let context = context.clone();
-		let pressed = pressed.clone();
-		let closure = Closure::wrap(Box::new(move |event: web_sys::MouseEvent| {
-			context.begin_path();
-			context.move_to(event.offset_x() as f64, event.offset_y() as f64);
-			pressed.set(true);
-		}) as Box<dyn FnMut(_)>);
-		canvas.add_event_listener_with_callback("mousedown", closure.as_ref().unchecked_ref())?;
-		closure.forget();
-	}
-	{
-		let context = context.clone();
-		let pressed = pressed.clone();
-		let closure = Closure::wrap(Box::new(move |event: web_sys::MouseEvent| {
-			if pressed.get() {
-				context.line_to(event.offset_x() as f64, event.offset_y() as f64);
-				context.stroke();
-				context.begin_path();
-				context.move_to(event.offset_x() as f64, event.offset_y() as f64);
-			}
-		}) as Box<dyn FnMut(_)>);
-		canvas.add_event_listener_with_callback("mousemove", closure.as_ref().unchecked_ref())?;
-		closure.forget();
-	}
-	{
-		let context = context.clone();
-		let pressed = pressed.clone();
-		let closure = Closure::wrap(Box::new(move |event: web_sys::MouseEvent| {
-			pressed.set(false);
+
+	Ok(canvas)
+}
+
+fn create_context(canvas: &Canvas) -> Result<Context, JsValue> {
+	let context = canvas.get_context("2d")?.unwrap().dyn_into::<Context>()?;
+	Ok(context)
+}
+
+fn mouse_down(context: Rc<Context>, canvas: &Canvas, pressed: Rc<Cell<bool>>) -> Result<(), JsValue> {
+	let closure = Closure::wrap(Box::new(move |event: web_sys::MouseEvent| {
+		context.begin_path();
+		context.move_to(event.offset_x() as f64, event.offset_y() as f64);
+		pressed.set(true);
+	}) as Box<dyn FnMut(_)>);
+
+	canvas.add_event_listener_with_callback("mousedown", closure.as_ref().unchecked_ref())?;
+	closure.forget();
+
+	Ok(())
+}
+
+fn mouse_move(context: Rc<Context>, canvas: &Canvas, pressed: Rc<Cell<bool>>) -> Result<(), JsValue> {
+	let closure = Closure::wrap(Box::new(move |event: web_sys::MouseEvent| {
+		if pressed.get() {
 			context.line_to(event.offset_x() as f64, event.offset_y() as f64);
 			context.stroke();
-		}) as Box<dyn FnMut(_)>);
-		canvas.add_event_listener_with_callback("mouseup", closure.as_ref().unchecked_ref())?;
-		closure.forget();
-	}
+			context.begin_path();
+			context.move_to(event.offset_x() as f64, event.offset_y() as f64);
+		}
+	}) as Box<dyn FnMut(_)>);
+
+	canvas.add_event_listener_with_callback("mousemove", closure.as_ref().unchecked_ref())?;
+	closure.forget();
+
+	Ok(())
+}
+
+fn mouse_up(context: Rc<Context>, canvas: &Canvas, pressed: Rc<Cell<bool>>) -> Result<(), JsValue> {
+	let closure = Closure::wrap(Box::new(move |event: web_sys::MouseEvent| {
+		pressed.set(false);
+		context.line_to(event.offset_x() as f64, event.offset_y() as f64);
+		context.stroke();
+	}) as Box<dyn FnMut(_)>);
+
+	canvas.add_event_listener_with_callback("mouseup", closure.as_ref().unchecked_ref())?;
+	closure.forget();
 
 	Ok(())
 }
